b8cd65038e8d8ef44049aa654c5a4c0402478d1b
Juan Pablo Romero <jpablo.romero@gmail.com>
2021-09-27 00:38:29 -0700

    changes

--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,12 +1,23 @@
 import zio.test.*
 import zio.test.Assertion.*
-
+// import zio.test.magnolia.DeriveGen
 
 object Test1Spec extends DefaultRunnableSpec {
 
   def spec = suite("suite")(
     test("test") {
       assert(1)(equalTo(1))
-    }
+    },
+//    testM("random") {
+//      check(generators.updateMetricRequest) { x =>
+//        println(x.asJson)
+//        assert(x)(equalTo(x))
+//      }
+//    }
   )
 }
+
+//object generators {
+//  val metricRequest = DeriveGen[MetricRequest]
+//  val updateMetricRequest = DeriveGen[UpdateMetricRequest]
+//}
