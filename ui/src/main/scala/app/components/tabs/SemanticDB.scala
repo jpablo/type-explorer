b4c8cc51997c2d34042f602fe167efd73f5e11e9
Juan Pablo Romero <jpablo.romero@gmail.com>
2022-06-09 19:51:04 -0700

    Show synthetics and Positions

--- ui/src/main/scala/app/components/tabs/SemanticDB.scala
+++ ui/src/main/scala/app/components/tabs/SemanticDB.scala
@@ -4,7 +4,7 @@ import bootstrap.Accordion.{`accordion-flush`, accordion, open}
 import com.raquo.laminar.api.L.*
 import models.Type
 import com.raquo.airstream.core.EventStream
-import scala.meta.internal.semanticdb.{TextDocument, SymbolInformation}
+import scala.meta.internal.semanticdb.{TextDocument, SymbolInformation, SymbolOccurrence, Synthetic}
 
 
 def semanticDBTab(documents: EventStream[List[TextDocument]]) =
@@ -25,12 +25,42 @@ object SemanticDB:
       div(
         cls := "symbol-information-container",
         children <-- elem.map(_.symbols).split(_.symbol)(renderSymbolInformation)
+      ),
+      div(
+        cls := "occurrences-container",
+        children <-- elem.map(_.occurrences).split(_.symbol)(renderOcurrence)
+      ),
+      div(
+        cls := "synthetics-container",
+        children <-- elem.map(_.synthetics).split(_.range.map(_.toProtoString).getOrElse(""))(renderSynthetics)
       )
     )
 
   def renderSymbolInformation(id: String, initial: SymbolInformation, elem: EventStream[SymbolInformation]) =
     div(
       cls := "card symbol-information",
+      div(
+        cls := "card-body",
+        pre(
+          child.text <-- elem.map(_.toProtoString)
+        )          
+      )
+    )
+  
+  def renderOcurrence(id: String, initial: SymbolOccurrence, elem: EventStream[SymbolOccurrence]) =
+    div(
+      cls := "card occurrence",
+      div(
+        cls := "card-body",
+        pre(
+          child.text <-- elem.map(_.toProtoString)
+        )          
+      )
+    )
+  
+  def renderSynthetics(id: String, initial: Synthetic, elem: EventStream[Synthetic]) =
+    div(
+      cls := "card synthetic",
       div(
         cls := "card-body",
         pre(
