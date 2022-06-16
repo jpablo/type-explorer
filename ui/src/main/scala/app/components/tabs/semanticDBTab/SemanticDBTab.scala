331bc27a438581b0f9838aecf0d906138f63b1a6
Juan Pablo Romero <jpablo.romero@gmail.com>
2022-06-15 17:03:32 -0700

    restructure code

--- ui/src/main/scala/app/components/tabs/semanticDBTab/SemanticDBTab.scala
+++ ui/src/main/scala/app/components/tabs/semanticDBTab/SemanticDBTab.scala
@@ -16,79 +16,22 @@ import com.raquo.airstream.core.Signal
 import scalajs.js.URIUtils.encodeURIComponent
 import app.components.tabs.semanticDBTab.SemanticDBStructureFlat
 import app.components.tabs.semanticDBTab.SemanticDBText
+import app.components.tabs.semanticDBTab.SemanticDBTree.*
 import widgets.{Icons, collapsable}
 import FileTree.*
 import util.Operators.*
-
 import scala.compiletime.ops.int.ToString
 import com.raquo.airstream.state.Val
 import jdk.jfr.consumer
 
 
-def renderTree[A]
-  (t: FileTree[A])
-  (
-    renderBranch: String => HtmlElement,
-    renderLeaf: (String, A) => HtmlElement
-  )
-: HtmlElement = t match
-  case Directory(name, files) =>
-    collapsable(
-      branchLabel = renderBranch(name),
-      $children =
-        Signal.fromValue {
-          for f <- files yield
-            renderTree(f)(renderBranch, renderLeaf)
-        },
-      open = true
-    )
-  case File(name, data) =>
-    renderLeaf(name, data)
-
-def renderSymbolInformation(id: String, initial: SymbolInformation, elem: Signal[SymbolInformation]) =
-  collapsable(
-    branchLabel = span(children <-- elem.map(sym =>
-      List(
-        span(sym.kind.toString),
-        span(": "),
-        a(href := "#" + encodeURIComponent(sym.symbol),  sym.displayName)
-      )
-    )),
-    $children = elem.map(sym =>
-      List(
-        li( "kind: "  + sym.kind ),
-        li( "symbol: "  + sym.symbol ),
-      )
-    )
-  )
-
 def semanticDBTab($documents: EventStream[List[TextDocumentsWithSource]]) =
-  val $tree =
-    for documents <- $documents yield
-      for fileTree <- FileTree.build(documents)(_.semanticDbUri) yield
-        renderTree(fileTree)(
-          renderBranch = b =>
-            span(cls := "collapsable-branch-label", b),
-
-          renderLeaf = (name, doc: TextDocumentsWithSource) =>
-            val cs =
-              for doc <- doc.documents yield
-                val $body = Signal.fromValue(doc).map(_.symbols.sortBy(_.symbol)).split(_.symbol)(renderSymbolInformation)
-                collapsable(
-                  span("uri: ", a(href := "#" + encodeURIComponent(doc.uri), doc.uri)),
-                  $body
-                )
-            collapsable(
-              branchLabel = span(cls := "collapsable-leaf", Icons.fileBinary, a(href := "#" + doc.semanticDbUri, name)),
-              $children = Signal.fromValue(cs)
-            )
-        )
   div(
     cls := "text-document-areas",
     div(
       cls := "structure",
       div(""), // TODO: add controls to expand / collapse all
-      children <-- $tree
+      children <-- SemanticDBTree.buildTree($documents)
 //      ol(
 //        children <-- documentsWithSource.split(_.semanticDbUri)(SemanticDBStructureFlat.structureLevel1),
 //      )
---
+++ ui/src/main/scala/app/components/tabs/semanticDBTab/SemanticDBTree.scala
@@ -0,0 +1,85 @@
+package app.components.tabs.semanticDBTab
+
+import app.components.tabs.semanticDBTab.FileTree.*
+import app.components.tabs.semanticDBTab.{SemanticDBStructureFlat, SemanticDBText}
+import bootstrap.Accordion.{`accordion-flush`, accordion, open}
+import com.raquo.airstream.core.{EventStream, Signal}
+import com.raquo.airstream.state.Val
+import com.raquo.laminar.api.L.*
+import com.raquo.laminar.nodes.ReactiveElement.Base
+import com.raquo.laminar.nodes.ReactiveHtmlElement
+import jdk.jfr.consumer
+import models.Type
+import org.jpablo.typeexplorer.TextDocumentsWithSource
+import org.scalajs.dom.html.LI
+import scalapb.GeneratedMessage
+import util.Operators.*
+import widgets.{Icons, collapsable}
+
+import scala.compiletime.ops.int.ToString
+import scala.meta.internal.semanticdb.{SymbolInformation, SymbolOccurrence, Synthetic, TextDocument}
+import scala.scalajs.js.URIUtils.encodeURIComponent
+
+
+object SemanticDBTree:
+
+  def buildTree($documents: EventStream[List[TextDocumentsWithSource]]): EventStream[List[HtmlElement]] =
+    for documents <- $documents yield
+      for fileTree <- FileTree.build(documents)(_.semanticDbUri) yield
+        renderTree(fileTree)(
+          renderBranch = b =>
+            span(cls := "collapsable-branch-label", b),
+
+          renderLeaf = (name, doc: TextDocumentsWithSource) =>
+            val cs =
+              for doc <- doc.documents yield
+                val $body = Signal.fromValue(doc).map(_.symbols.sortBy(_.symbol)).split(_.symbol)(renderSymbolInformation)
+                  collapsable(
+                  span("uri: ", a(href := "#" + encodeURIComponent(doc.uri), doc.uri)),
+                  $body
+                )
+            collapsable(
+              branchLabel = span(cls := "collapsable-leaf", Icons.fileBinary, a(href := "#" + doc.semanticDbUri, name)),
+              $children = Signal.fromValue(cs)
+            )
+        )
+
+
+  def renderTree[A]
+    (t: FileTree[A])
+    (
+      renderBranch: String => HtmlElement,
+      renderLeaf: (String, A) => HtmlElement
+    )
+  : HtmlElement = t match
+    case Directory(name, files) =>
+      collapsable(
+        branchLabel = renderBranch(name),
+        $children =
+          Signal.fromValue {
+            for f <- files yield
+              renderTree(f)(renderBranch, renderLeaf)
+          },
+        open = true
+      )
+    case File(name, data) =>
+      renderLeaf(name, data)
+
+  def renderSymbolInformation(id: String, initial: SymbolInformation, elem: Signal[SymbolInformation]) =
+    collapsable(
+      branchLabel = span(children <-- elem.map(sym =>
+        List(
+          span(sym.kind.toString),
+          span(": "),
+          a(href := "#" + encodeURIComponent(sym.symbol),  sym.displayName)
+        )
+      )),
+      $children = elem.map(sym =>
+        List(
+          li( "kind: "  + sym.kind ),
+          li( "symbol: "  + sym.symbol ),
+        )
+      )
+    )
+
+end SemanticDBTree
