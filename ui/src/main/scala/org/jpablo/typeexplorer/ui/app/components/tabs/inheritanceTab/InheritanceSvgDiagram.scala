2bad1cc4f3ee2ede048748260b9e7a7617138765
Juan Pablo Romero <jpablo.romero@gmail.com>
2023-03-22 17:05:27 -0700

    disable upload dependency graph for now

--- ui/src/main/scala/org/jpablo/typeexplorer/ui/app/components/tabs/inheritanceTab/InheritanceSvgDiagram.scala
+++ ui/src/main/scala/org/jpablo/typeexplorer/ui/app/components/tabs/inheritanceTab/InheritanceSvgDiagram.scala
@@ -9,16 +9,16 @@ class InheritanceSvgDiagram(svg: dom.SVGElement):
   svg.removeStyle("background")
   // (more styles are set in style.scss)
 
-  private def setDimensions(w: Int, h: Int) =
+  private def setDimensions(w: Int, h: Int): Unit =
     svg.setStyle("width" -> s"${w}px", "height" -> s"${h}px")
 
   private def width = svg.getBoundingClientRect().width
   private def height = svg.getBoundingClientRect().height
 
-  def zoom(r: Double) =
+  def zoom(r: Double): Unit =
     setDimensions((width * r).toInt, (height * r).toInt)
 
-  def fitToRect(rect: dom.DOMRect) =
+  def fitToRect(rect: dom.DOMRect): Unit =
     zoom(scala.math.min(rect.width / width, rect.height / height))
 
   private def selectableElements =
@@ -36,15 +36,15 @@ class InheritanceSvgDiagram(svg: dom.SVGElement):
   def elementSymbols: Set[models.Symbol] =
     namespaceElements.map(_.symbol).toSet
 
-  def select(symbols: Set[models.Symbol]) =
+  def select(symbols: Set[models.Symbol]): Unit =
     for elem <- selectableElements if symbols.contains(elem.symbol) do
       elem.select()
 
-  def unselectAll() =
+  def unselectAll(): Unit =
     selectableElements.foreach(_.unselect())
 
   def toLaminar =
-    new ChildNode[dom.SVGElement] { val ref = svg }
+    foreignSvgElement(svg)
 
 
 object InheritanceSvgDiagram:
