db5999a04f58076955432ccbfdbc64c40c894765
Juan Pablo Romero <jpablo.romero@gmail.com>
2023-09-21 08:30:25 -0700

    using pathname from window.location

--- ui/src/main/scala/org/jpablo/typeexplorer/ui/app/MainJS.scala
+++ ui/src/main/scala/org/jpablo/typeexplorer/ui/app/MainJS.scala
@@ -5,14 +5,14 @@ import org.jpablo.typeexplorer.ui.app.client.{fetchDocuments, fetchInheritanceDi
 import org.jpablo.typeexplorer.ui.app.components.TopLevel
 import org.jpablo.typeexplorer.ui.app.components.state.AppState
 import org.jpablo.typeexplorer.ui.app.components.tabs.inheritanceTab.InheritanceSvgDiagram
-import org.scalajs.dom.document
+import org.scalajs.dom
 
 object MainJS:
 
   def main(args: Array[String]): Unit =
-    val projectId = "e96495a0-6f86-41ab-bfc0-390063ac7326"
-    val appState = AppState.load(fetchInheritanceDiagram, Some(projectId))
+    val projectId = dom.window.location.pathname.split("/").lastOption
+    val appState = AppState.load(fetchInheritanceDiagram, projectId)
     val documents = fetchDocuments(appState.basePaths)
     val inheritanceSvgDiagram = fetchInheritanceSVGDiagram(appState).startWith(InheritanceSvgDiagram.empty)
     val app = TopLevel(appState, inheritanceSvgDiagram, documents)
-    render(document.querySelector("#app"), app)
+    render(dom.document.querySelector("#app"), app)
