d852e7442031b877bc7426e70f847e53cb050c6b
Alexei Diaz <alexei.eleusis@gmail.com>
2023-10-04 17:49:37 -0700

    Fixes PlantumlInheritanceSpec

--- shared/src/test/scala/org/jpablo/typeexplorer/shared/inheritance/PlantumlInheritanceSpec.scala
+++ shared/src/test/scala/org/jpablo/typeexplorer/shared/inheritance/PlantumlInheritanceSpec.scala
@@ -6,15 +6,177 @@ import zio.json.*
 
 object PlantumlInheritanceSpec extends ZIOSpecDefault {
 
-  val json = io.Source.fromInputStream(getClass.getResourceAsStream("/example-inheritance-diagram.json")).mkString.strip
-  val diagram = json.fromJson[InheritanceDiagram].toOption.get
+  val diagram = exampleInheritanceDiagram.fromJson[InheritanceDiagram].toOption.get
 
   def spec =
     suite("PlantumlInheritance")(
       test("export to plantuml"):
         val uml = diagram.toPlantUML(Map.empty, DiagramOptions(showFields = true))
-        val expected = io.Source.fromInputStream(getClass.getResourceAsStream("/example.puml")).mkString.strip
+        val expected = examplePuml.strip
 
         assertTrue(uml.diagram == expected)
     )
 }
+
+val examplePuml =
+  """
+    |@startuml
+    |set namespaceSeparator none
+    |skinparam class {
+    |  'FontSize 20
+    |  'FontName "JetBrains Mono"
+    |}
+    |
+    |'declarations
+    |
+    |
+    |namespace "bank.b2.core.api.kafka" as bank.b2.core.api.kafka {
+    |  class "AccountClosed" as bank/b2/core/api/kafka/AccountEvent.AccountClosed# {
+    |  accountId
+    |' bank/b2/core/api/kafka/AccountEvent.AccountClosed#accountId.
+    |  externalId
+    |' bank/b2/core/api/kafka/AccountEvent.AccountClosed#externalId.
+    |  metadata
+    |' bank/b2/core/api/kafka/AccountEvent.AccountClosed#metadata.
+    |  timestamp
+    |' bank/b2/core/api/kafka/AccountEvent.AccountClosed#timestamp.
+    |}
+    |
+    |class "AccountEvent" as bank/b2/core/api/kafka/AccountEvent# << (T, pink) >> {
+    |  accountId
+    |' bank/b2/core/api/kafka/AccountEvent#accountId().
+    |  externalId
+    |' bank/b2/core/api/kafka/AccountEvent#externalId().
+    |  metadata
+    |' bank/b2/core/api/kafka/AccountEvent#metadata().
+    |  timestamp
+    |' bank/b2/core/api/kafka/AccountEvent#timestamp().
+    |}
+    |
+    |}
+    |
+    |
+    |'inheritance
+    |
+    |"bank/b2/core/api/kafka/AccountEvent#" <|-- "bank/b2/core/api/kafka/AccountEvent.AccountClosed#"
+    |@enduml
+    |""".stripMargin
+
+val exampleInheritanceDiagram =
+  """
+    |{
+    |  "arrows": [
+    |    [
+    |      "bank/b2/core/api/kafka/AccountEvent.AccountClosed#",
+    |      "bank/b2/core/api/kafka/AccountEvent#"
+    |    ]
+    |  ],
+    |  "namespaces": [
+    |    {
+    |      "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#",
+    |      "displayName": "AccountClosed",
+    |      "kind": {
+    |        "Class": {}
+    |      },
+    |      "methods": [
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#accountId.",
+    |          "displayName": "accountId"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#canEqual().",
+    |          "displayName": "canEqual"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#copy().",
+    |          "displayName": "copy"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#equals().",
+    |          "displayName": "equals"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#externalId.",
+    |          "displayName": "externalId"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#hashCode().",
+    |          "displayName": "hashCode"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#metadata.",
+    |          "displayName": "metadata"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#productArity().",
+    |          "displayName": "productArity"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#productElement().",
+    |          "displayName": "productElement"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#productIterator().",
+    |          "displayName": "productIterator"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#productPrefix().",
+    |          "displayName": "productPrefix"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#timestamp.",
+    |          "displayName": "timestamp"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent.AccountClosed#toString().",
+    |          "displayName": "toString"
+    |        }
+    |      ],
+    |      "documentURI": "b2-banking-platform-api/src/main/scala/bank/b2/core/api/kafka/AccountEvent.scala",
+    |      "semanticDbUri": "/Users/jpablo/chamba/backend/b2-banking-platform-api/target/scala-2.12/meta/META-INF/semanticdb/b2-banking-platform-api/src/main/scala/bank/b2/core/api/kafka/AccountEvent.scala.semanticdb",
+    |      "basePath": "/Users/jpablo/chamba/backend/b2-banking-platform-api",
+    |      "range": {
+    |        "startLine": 70,
+    |        "startChar": 13,
+    |        "endLine": 70,
+    |        "endChar": 26
+    |      }
+    |    },
+    |    {
+    |      "symbol": "bank/b2/core/api/kafka/AccountEvent#",
+    |      "displayName": "AccountEvent",
+    |      "kind": {
+    |        "Trait": {}
+    |      },
+    |      "methods": [
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent#accountId().",
+    |          "displayName": "accountId"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent#externalId().",
+    |          "displayName": "externalId"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent#metadata().",
+    |          "displayName": "metadata"
+    |        },
+    |        {
+    |          "symbol": "bank/b2/core/api/kafka/AccountEvent#timestamp().",
+    |          "displayName": "timestamp"
+    |        }
+    |      ],
+    |      "documentURI": "b2-banking-platform-api/src/main/scala/bank/b2/core/api/kafka/AccountEvent.scala",
+    |      "semanticDbUri": "/Users/jpablo/chamba/backend/b2-banking-platform-api/target/scala-2.12/meta/META-INF/semanticdb/b2-banking-platform-api/src/main/scala/bank/b2/core/api/kafka/AccountEvent.scala.semanticdb",
+    |      "basePath": "/Users/jpablo/chamba/backend/b2-banking-platform-api",
+    |      "range": {
+    |        "startLine": 59,
+    |        "startChar": 12,
+    |        "endLine": 59,
+    |        "endChar": 24
+    |      }
+    |    }
+    |  ]
+    |}
+    |
+    """.stripMargin
