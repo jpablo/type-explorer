885d4925d10e95b2e23350ea36b7dac3644d66f4
Juan Pablo Romero <jpablo.romero@gmail.com>
2023-10-11 23:40:17 -0700

    semanticdbVersion="4.8.2"

--- scripts/compile-project.sh
+++ scripts/compile-project.sh
@@ -2,10 +2,10 @@
 
 project=${1:-""}
 
-semanticdbVersion="4.8.2"
+semanticdbVersion="4.8.11"
 
 if [ -z "$project" ]; then
-  sbt 'set ThisBuild / semanticdbEnabled := true; clean; compile'
+  sbt "set ThisBuild / semanticdbEnabled := true; set semanticdbVersion := \"$semanticdbVersion\"; clean; compile"
 else
   sbt "project $project; set semanticdbEnabled := true; set semanticdbVersion := \"$semanticdbVersion\"; clean; compile; Test / compile"
 fi
