ab0a4c6a50a74c5fe785244c9cc9468dcb4e70be
Juan Pablo Romero <jpablo.romero@gmail.com>
2024-01-07 12:18:27 -0800

    CSS simplification

--- ui/src/main/scala/org/jpablo/typeexplorer/ui/app/components/tabs/inheritanceTab/CanvasContainer.scala
+++ ui/src/main/scala/org/jpablo/typeexplorer/ui/app/components/tabs/inheritanceTab/CanvasContainer.scala
@@ -1,7 +1,6 @@
 package org.jpablo.typeexplorer.ui.app.components.tabs.inheritanceTab
 
 import com.raquo.airstream.core.{EventStream, Signal}
-import com.raquo.laminar.api.L
 import com.raquo.laminar.api.L.*
 import org.jpablo.typeexplorer.ui.app.components.state.CanvasSelectionOps
 import org.jpablo.typeexplorer.ui.app.components.tabs.inheritanceTab.svgGroupElement.*
@@ -14,36 +13,40 @@ def CanvasContainer(
     zoomValue:             Var[Double],
     fitDiagram:            EventStream[Unit]
 ) =
-  val svgWidth: Signal[Double] =
-    zoomValue.signal.combineWith(inheritanceSvgDiagram).map(_ * _.origW)
+  val svgSize =
+    zoomValue.signal
+      .combineWith(inheritanceSvgDiagram)
+      .map((z, diagram) => (z * diagram.origW, z * diagram.origH))
   div(
     cls             := "te-parent p-1 z-10",
     backgroundImage := "radial-gradient(oklch(var(--bc)/.2) .5px,oklch(var(--b2)/1) .5px)",
     backgroundSize  := "5px 5px",
-
     onClick.preventDefault
       .compose(_.withCurrentValueOf(inheritanceSvgDiagram)) --> handleSvgClick(canvasSelection).tupled,
-
     inContext { svgParent =>
-      def parentWidth() = svgParent.ref.offsetWidth
+      def parentSizeNow() = (svgParent.ref.offsetWidth, svgParent.ref.offsetHeight)
       // scale the diagram to fit the parent container whenever the "fit" button is clicked
       fitDiagram
-        .sample(inheritanceSvgDiagram.map(_.origW))
-        .foreach { svgOrigWidth =>
-          zoomValue.set(parentWidth() / svgOrigWidth)
+        .sample(inheritanceSvgDiagram)
+        .foreach { diagram =>
+          val (parentWidth, parentHeight) = parentSizeNow()
+          val z = math.min(parentWidth / diagram.origW, parentHeight / diagram.origH)
+          zoomValue.set(z)
           // TODO: is there a way to avoid unsafeWindowOwner here?
         }(unsafeWindowOwner)
 
-      // TODO: Is there a better way to get a signal out of the onResize event?
-      val parentWidthS =
-        windowEvents(_.onResize).mapTo(parentWidth()).startWith(parentWidth())
       // Center the diagram *only* when the parent container is wider than the svg diagram.
       // Otherwise the left side of the diagram is not accessible with scroll bars (as they only extend to the right).
       val flexJustification =
-        parentWidthS
-          .combineWith(svgWidth)
-          .map: (w, svgWidth) =>
-            if w < svgWidth then "justify-start" else "justify-center"
+        windowEvents(_.onResize).mapToUnit
+          .startWith(())
+          .combineWith(svgSize)
+          .map: (svgWidth, svgHeight) =>
+            val (parentWidth, parentHeight) = parentSizeNow()
+            Seq(
+              if parentWidth < svgWidth then "justify-start" else "justify-center",
+              if parentHeight < svgHeight then "items-start" else "items-center"
+            )
       Seq(
         cls <-- flexJustification,
         child <-- inheritanceSvgDiagram.map: diagram =>
@@ -53,7 +56,8 @@ def CanvasContainer(
           canvasSelection.remove(selection -- diagram.elementSymbols)
 
           diagram.toLaminar.amend(
-            svg.width <-- svgWidth.map(_.toString + "px")
+            svg.width <-- svgSize.map(_._1.toString + "px"),
+            svg.height <-- svgSize.map(_._2.toString + "px")
           )
       )
     }
